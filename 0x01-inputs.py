7d42f8d05220da832af30ce6024808e47fbbae52
EvansOdhams <evansodhams@gmail.com>
2023-06-13 10:54:23 +0300

    Add Python script to capture and print user input

---
+++ 0x01-inputs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a Class"""
+
+
+"""Prompt the user for input"""
+user_input = input("Enter your username: ")
+
+"""Print the user's input"""
+print("Your Name is: ", user_input)
