19f827d241b0e429e88fe5c4a1146891cb626e07
EvansOdhams <evansodhams@gmail.com>
2023-06-21 15:44:24 +0300

    Add value evaluation in python

---
+++ 0x02-strings/booleans/true_false.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
---
+++ 0x02-strings/booleans/value_evaluation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+print(bool("Hello"))
+print(bool(15))
