8af0352b00b7357e966639f485101af85ec2177e
EvansOdhams <evansodhams@gmail.com>
2023-06-23 13:49:22 +0300

    Add 3-iteration.py

---
+++ python-dictionaries/3-iteration.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+#for loop to print the items of the dictionary by using items() method    
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"WIPRO"}       
+for x in Employee.items():        
+    print(x)      
