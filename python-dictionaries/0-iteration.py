0f3e041791c29e9ea05de77c4bc89bc67ce97114
EvansOdhams <evansodhams@gmail.com>
2023-06-23 13:44:32 +0300

    Add 0-iteration.py

---
+++ python-dictionaries/0-iteration.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+# for loop to print all the keys of a dictionary    
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"WIPRO"}        
+for x in Employee:        
+    print(x)       
