09693725ad80f4f5abe1c21c6040c69bdad642ee
EvansOdhams <evansodhams@gmail.com>
2023-06-23 09:12:29 +0300

    1-adding_dict_values.py

---
+++ python-dictionaries/1-adding_dict_values.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+Employee = {"Name": "Dev", "Age": 20, "salary":45000,"Company":"WIPRO"}         
+print(type(Employee))        
+print("printing Employee data .... ")        
+print(Employee)        
+print("Enter the details of the new employee....");        
+Employee["Name"] = input("Name: ");        
+Employee["Age"] = int(input("Age: "));        
+Employee["salary"] = int(input("Salary: "));        
+Employee["Company"] = input("Company:");        
+print("printing the new data");        
+print(Employee)     
