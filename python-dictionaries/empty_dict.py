89ab6d10e78045ed1c22b61b5d989cb0f85e4607
EvansOdhams <evansodhams@gmail.com>
2023-06-23 08:49:06 +0300

    Add empty_dict.py

---
+++ python-dictionaries/empty_dict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+# Creating an empty Dictionary       
+Dict = {}       
+print("Empty Dictionary: ")       
+print(Dict)       
+      
+# Creating a Dictionary       
+# with dict() method       
+Dict = dict({1: 'Hcl', 2: 'WIPRO', 3:'Facebook'})       
+print("\nCreate Dictionary by using  dict(): ")       
+print(Dict)       
+      
+# Creating a Dictionary       
+# with each item as a Pair       
+Dict = dict([(4, 'Rinku'), (2, Singh)])       
+print("\nDictionary with each item as a pair: ")       
+print(Dict)       
