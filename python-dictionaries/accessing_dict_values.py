f477459f6ca08264395b145bcdb171e5e4e47602
EvansOdhams <evansodhams@gmail.com>
2023-06-23 08:51:14 +0300

    Add accessing_dict_values.py

---
+++ python-dictionaries/accessing_dict_values.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+Employee = {"Name": "Dev", "Age": 20, "salary":45000,"Company":"WIPRO"}      
+print(type(Employee))      
+print("printing Employee data .... ")      
+print("Name : %s" %Employee["Name"])      
+print("Age : %d" %Employee["Age"])      
+print("Salary : %d" %Employee["salary"])      
+print("Company : %s" %Employee["Company"])       
