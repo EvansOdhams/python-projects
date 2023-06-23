98d98b15b09f56af22741b2cd7c868cd44a9187c
EvansOdhams <evansodhams@gmail.com>
2023-06-23 13:48:11 +0300

    Add 2-iteration.py

---
+++ python-dictionaries/2-iteration.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+#for loop to print the values of the dictionary by using values() method.    
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"WIPRO"}        
+for x in Employee.values():        
+    print(x)       
