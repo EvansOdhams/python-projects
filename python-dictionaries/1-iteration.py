3fb83eebef332a78246626fc957cfc35820f437a
EvansOdhams <evansodhams@gmail.com>
2023-06-23 13:46:57 +0300

    Add 1-iteration.py

---
+++ python-dictionaries/1-iteration.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+#for loop to print all the values of the dictionary    
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"WIPRO"} for x in Employee:        
+ print(Employee[x])       
