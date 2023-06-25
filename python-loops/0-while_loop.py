86d595a01eaedbe5f589ec9891f95c076ed85c6d
EvansOdhams <evansodhams@gmail.com>
2023-06-25 18:19:22 +0300

    Add 0-while_loop.py

---
+++ python-loops/0-while_loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+# Python program to show how to use a while loop  
+counter = 0  
+# Initiating the loop  
+while counter < 10: # giving the condition  
+    counter = counter + 3  
+    print("Python Loops")  
