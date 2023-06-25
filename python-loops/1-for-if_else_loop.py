4bcdcdb9add27ead5f21db1e2231f394d7832c4b
EvansOdhams <evansodhams@gmail.com>
2023-06-25 18:16:25 +0300

    Add 1-for-if_else_loop.py

---
+++ python-loops/1-for-if_else_loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+# Python program to show how to use else statement with for loop  
+  
+# Creating a sequence  
+tuple_ = (3, 4, 6, 8, 9, 2, 3, 8, 9, 7)  
+  
+# Initiating the loop  
+for value in tuple_:  
+    if value % 2 != 0:  
+        print(value)  
+# giving an else statement  
+else:  
+    print("These are the odd numbers present in the tuple")  
