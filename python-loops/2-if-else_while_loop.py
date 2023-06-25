bdbea6450b71ebfd40e659febd987e54790dc542
EvansOdhams <evansodhams@gmail.com>
2023-06-25 18:20:45 +0300

    Add 2-if-else_while_loop.py

---
+++ python-loops/2-if-else_while_loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+#Python program to show how to use else statement with the while loop  
+counter = 0  
+  
+# Iterating through the while loop  
+while (counter < 10):      
+    counter = counter + 3  
+    print("Python Loops") # Executed untile condition is met  
+# Once the condition of while loop gives False this statement will be executed  
+else:  
+    print("Code block inside the else statement")  
