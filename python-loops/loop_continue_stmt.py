47cd671e194c11e023c17831246337148b86c924
EvansOdhams <evansodhams@gmail.com>
2023-06-25 18:43:06 +0300

    Add loop_continue_stmt.py

---
+++ python-loops/loop_continue_stmt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+# Python program to show how the continue statement works  
+  
+# Initiating the loop  
+for string in "Python Loops":  
+    if string == "o" or string == "p" or string == "t":  
+         continue  
+    print('Current Letter:', string)  
