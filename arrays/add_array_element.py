55fb0838d3881cb51812a02e40d7147c515e3bd1
EvansOdhams <evansodhams@gmail.com>
2023-06-26 12:12:26 +0300

    Add delete array elements script

---
+++ arrays/add_array_element.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+cars = ["Ford", "BMW", "Volkswagen"]
+
+cars.append("Honda")
+
+print(cars)
---
+++ arrays/remove_array_element.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+cars = ["Toyota", "Mazda", "Mitsubishi"]
+
+cars.append("Honda")
+cars.pop(1)
+print(cars)
