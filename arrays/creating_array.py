09673aebdcc92bc7a37bccc36255092b4989e22d
EvansOdhams <evansodhams@gmail.com>
2023-06-26 05:57:17 +0300

    Add creating_array.py

---
+++ arrays/creating_array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+import array as arr  
+a = arr.array('i', [2, 4, 6, 8])  
+print("First element:", a[0])  
+print("Second element:", a[1])  
+print("Second last element:", a[-1])  
