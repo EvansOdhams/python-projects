604f9ba53d74272eba024868e3c727efb0200207
EvansOdhams <evansodhams@gmail.com>
2023-06-26 13:27:50 +0300

    Edit 0-create_str_function.py

--- classes_and_objects/0-create_str_function.py
+++ classes_and_objects/0-create_str_function.py
@@ -2,9 +2,12 @@
 
 
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Person(name={self.name}, age={self.age})"
 
 p1 = Person("John", 36)
 
