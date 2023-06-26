6d3b3198ef048316e0b46523de4da7e20b252d6d
EvansOdhams <evansodhams@gmail.com>
2023-06-26 21:57:23 +0300

    Add method_overriding.py

---
+++ python-inheritance/method_overriding.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class BaseClassName:
+    def some_method(self):
+        # base class implementation
+
+class DerivedClassName(BaseClassName):
+    def some_method(self):
+        # derived class implementation
