9f4a45469f43aadba72240b385d7ac514fac5f97
EvansOdhams <evansodhams@gmail.com>
2023-06-26 12:59:10 +0300

    Add create-class_and_functions.py

---
+++ classes_and_objects/create-class_and_functions.py
@@ -0,0 +1,7 @@
+class MyClass:
+    my_attribute = "Hello World!"  # Define a class attribute
+
+my_object = MyClass()  # Create an instance of the class
+
+# Access and print the class attribute using the class name
+print(MyClass.my_attribute)
