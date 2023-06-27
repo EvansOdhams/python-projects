3facdb7170ddadf0b9a65025babe167e2cabf6e2
EvansOdhams <evansodhams@gmail.com>
2023-06-27 18:20:20 +0300

    Add 0-implement_math_function.py

---
+++ python-math/0-implement_math_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+import math
+
+x = 4
+y = 2
+
+print(math.sqrt(x))         # Output: 2.0
+print(math.pow(x, y))       # Output: 16.0
+print(math.sin(math.pi/2))  # Output: 1.0
