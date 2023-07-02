eeb51efc2d40dca4add9a059b886ec99964e453b
EvansOdhams <evansodhams@gmail.com>
2023-07-02 12:56:55 +0300

    Add 4-user_input.py

---
+++ user-input/4-user_input.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
