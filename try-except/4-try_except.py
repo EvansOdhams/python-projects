108127308a78cc280cb0016720122226a1a77597
EvansOdhams <evansodhams@gmail.com>
2023-07-02 11:24:48 +0300

    Add 4-try_except.py

---
+++ try-except/4-try_except.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
