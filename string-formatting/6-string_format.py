8ea3a2e81c58ff37d6015bf90a3bbe3832c1e947
EvansOdhams <evansodhams@gmail.com>
2023-07-02 13:11:45 +0300

    Add 6-string_format.py

---
+++ string-formatting/6-string_format.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+Concatenated: Name: Alice, Age: 30, Height: 1.65
+Interpolated: Name: Alice, Age: 30, Height: 1.65
+Formatted: Name: Alice, Age: 30, Height: 1.65
+F-string: Name: Alice, Age: 30, Height: 1.65
